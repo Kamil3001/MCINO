3ee2e882c211aa6fbb838c2f166ebe48b14f2ec8
Kamil <kamil.cierpisz@ucdconnect.ie>
2019-02-09 14:13:53 +0000

    Added the backbone of Metrics class

--- src/metrics/Metrics.java
+++ src/metrics/Metrics.java
@@ -1,4 +1,42 @@
 package metrics;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 public class Metrics {
+    private Class cls;
+    private int numOfPublicFields;
+    private int numOfPublicMethods;
+    private int classLength;
+    private HashMap<Method,Integer> methodLengths;
+    private HashMap<Method, Integer> paramsPerMethod;
+
+    public Metrics(Class cls){
+        this.cls = cls;
+        extractMetrics();
+    }
+
+    private void extractMetrics(){
+
+    }
+
+    public int getNumOfPublicFields() {
+        return numOfPublicFields;
+    }
+
+    public int getNumOfPublicMethods() {
+        return numOfPublicMethods;
+    }
+
+    public int getClassLength() {
+        return classLength;
+    }
+
+    public HashMap<Method, Integer> getMethodLengths() {
+        return methodLengths;
+    }
+
+    public HashMap<Method, Integer> getParamsPerMethod() {
+        return paramsPerMethod;
+    }
 }
