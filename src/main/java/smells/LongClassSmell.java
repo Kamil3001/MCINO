66ba6df0a35918808d585db468356cec1c0668ad
Kamil <kamil.cierpisz@ucdconnect.ie>
2019-04-25 17:16:40 +0100

    LongClassSmell Tests implemented

--- src/main/java/smells/LongClassSmell.java
+++ src/main/java/smells/LongClassSmell.java
@@ -9,11 +9,11 @@ public class LongClassSmell extends AbstractCodeSmell {
     public void detectSmell(FileMetrics metrics) {
         int mainClassLength = metrics.getClassLengths().get(0);
 
-        if(mainClassLength > 1300)
+        if(mainClassLength > 1200)
             severity = 3;
-        else if(mainClassLength > 1150)
-            severity = 2;
         else if(mainClassLength > 1000)
+            severity = 2;
+        else if(mainClassLength > 800)
             severity = 1;
     }
 
